2453baecc1c74b77f69a22d1c0fd734143bff47a
AaronVillalobos <avilla4537@gmail.com>
2024-10-14 14:10:09 -0700

    Format district page

--- app/district/page.js
+++ app/district/page.js
@@ -1,18 +1,27 @@
 import Image from "next/image";
+import styles from "@/app/route-modules/district.css";
+import Link from "next/link";
 
 function District() {
     return (
         <div className="Map">
             <main>
-                <div className="map-search">Do you live in Trustee Area 5?</div>
-                <p>Find out Below</p>
-                <Image
-                    className="district-5-map"
-                    src="/images/district-5-map.png"
-                    width = {700}
-                    height = {700}
-                    alt = 'District 5 map'
-                />
+                <div className="map-container">
+                    <div className="map-search">DO YOU LIVE IN TRUSTEE AREA 5?</div>
+                    <div className="find-out">Find out below!</div>
+                    <div className="map-pic">
+                        <Image
+                            className="district-5-map"
+                            src="/images/district-5-map.png"
+                            width = {700}
+                            height = {700}
+                            alt = 'District 5 map'
+                        />
+                    </div>
+                    <div className="registration">
+                        <div className="reminder">Voter registration deadline is October 21st.</div> <div className="click-link"><span className='vote-link'><Link href="https://registertovote.ca.gov/">Click here</Link></span> to register to vote or check your voter registration status.</div>
+                    </div>
+                </div>
             </main>
         </div>
     )
