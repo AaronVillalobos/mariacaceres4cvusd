fd727b160866a75b2629ca36090bbb05938cf741
AaronVillalobos <avilla4537@gmail.com>
2024-10-21 13:15:55 -0700

    Update home.css

--- app/page.js
+++ app/page.js
@@ -17,7 +17,7 @@ function Home() {
             <div className='about-header'>About Maria</div>
             <div className='about-message'>
               <div>I have been a resident of the district for over 19 years. My family attended Grovecenter Elementary, Traweek Middle School, and graduated from Covina High School and South Hills High School.</div>
-              <div>From a very early age, I dreamed of becoming a teacher and learned early on that an education would be the key to transforming my dream into a reality. After graduating from high school, I worked my way through college working with children with special needs. I am the first in my family to obtain a bachelor’s degree and two master’s degrees. I have had the honor of teaching grades 1,2,5,6,7 and 8th and this year I have the pleasure of teaching 7th/8th graders.</div>
+              <div>From a very early age, I dreamed of becoming a teacher and learned early on that an education would be the key to transforming my dream into a reality. After graduating from high school, I worked my way through college working with children with special needs. I am the first in my family to obtain a bachelor’s degree and two master’s degrees. I have had the honor of teaching grades 1, 2, 5, 6, 7 and 8th and this year I have the pleasure of teaching 7th/8th graders.</div>
               <div>My 20 years of experience as an educator has given me invaluable insights to the needs of students, parents, and school staff. As your current school board trustee, I have used my background and experience in education to be an effective and proactive leader.</div>
             </div>
         </div>
