03139d6eb3bd3daa437edda77b529b950b7c69b9
AaronVillalobos <avilla4537@gmail.com>
2024-10-25 15:47:05 -0700

    Apply contact form

--- app/contact/page.js
+++ app/contact/page.js
@@ -30,11 +30,18 @@ function Contact() {
             <div className="email-form">
                 <form ref={form} onSubmit={sendEmail}>
                     <label>Name</label>
-                    <input type="text" name="user_name"/>
+                    <input className="subject" type="text" name="from_name" required/>
                     <label>Email</label>
-                    <input type="text" name="user_email"/>
+                    <input className="subject" type="email" name="user_email" required/>
+                    <label>Phone Number</label>
+                    <input className="subject" type="tel" name="phone_number" placeholder="XXX-XXX-XXXX" pattern="[0-9]{3}-[0-9]{3}-[0-9]{4}" required/>
                     <label>Message to Maria</label>
-                    <textarea name="message"/>
+                    <textarea name="message" required/>
+                    <div className="phone-checkbox">
+                        <input  id="opt-in" type="checkbox" name="opt" required/>
+                        <label for="opt-in">Opt-in to Receive Text Messages</label>
+                    </div>
+                    <div className="agreement">By providing your mobile phone number, you are giving your consent to receive calls and SMS/MMS messages to the number from Caceres for School Board 2024. Msg frequency varies. Msg & data rates may apply.</div>
                     <button type="submit">Submit</button>
                 </form>
             </div>
